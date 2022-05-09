c2e7bd07217b8f787b099a667df3a33ee3c9320c
srz_zumix <zumix.cpp@gmail.com>
2022-05-10 07:30:14 +0900

    up

--- tests/szCreateQRCode/job.groovy
+++ tests/szCreateQRCode/job.groovy
@@ -1,3 +1,8 @@
+job('Test_szCreateQRCode') {
+  definition {
+    cps {
+        sandbox(true)
+        script("""
 @Library(srz-zumix-shared-library) _
 
 pipeline {
@@ -8,6 +13,10 @@ pipeline {
       steps {
         szCreateQRCode("https://github.com/srz-zumix/jenkins_shared_library", 'qrcode.png', widht: 200, height: 200)
       }
+    }
+  }
+}
+""")
     }
   }
 }
